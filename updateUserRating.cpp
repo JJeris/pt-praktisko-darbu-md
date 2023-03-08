bd2011208cb5c1a546686d9c5bc8a189f36840b9
askolds <askolds11@gmail.com>
2023-03-08 22:57:04 +0200

    Added database simulations, comments

--- updateUserRating.cpp
+++ updateUserRating.cpp
@@ -2,6 +2,12 @@
 #include <list>
 using namespace std;
 
+// UserID, ko liktu queryā.
+// Sarakstā nebūtu jāeksistē cita tipa ID!
+// funkcijā iegūtu sarakstu un lietotāju tikai ar šo ID, ja būtu DB
+const int USERID = 2;
+
+// Cenas piedāvājuma objekts
 struct priceSuggestion {
   int rating;
   int userID;
@@ -10,39 +16,80 @@ struct priceSuggestion {
     this->rating = rating;
   }
 };
-list<priceSuggestion> priceSuggestions;
 
-void updateUserRating(int userID)
-{
-    if(priceSuggestions.size() == 0){
-        cout<<"Rating:0 (length==0)"<<endl;
+// Lietotāja objekts
+struct User {
+    int userID;
+    int userRating;
+    User(int userID, int userRating) {
+        this->userID = userID;
+        this->userRating = userRating;
+    }
+};
+
+// query = $"SELECT * FROM priceSuggestions WHERE userID = $userID"
+// query2 = $"SELECT * FROM Users WHERE userID = $userID"
+// simulē query, ja būtu īsta datubāze
+list<priceSuggestion> priceSuggestions;
+User userGlobal(USERID, 10);
+
+void updateUserRating(int userID) {
+    if(userID != USERID) { // Simulācija, ka neeksistē USER
+        cout << "WARNING: Tried to update user rating for a user which does not exist!" << endl; 
         return;
     }
+
+    User user = userGlobal; // query priekš user
+    list<priceSuggestion> userPriceSuggestions = priceSuggestions; // query priekš cenu piedāvājumiem
     int userRating = 0;
-    list<priceSuggestion> userPriceSuggestions = priceSuggestions; // Analog "select where userID == ..."
-    for (list<priceSuggestion>::iterator it=userPriceSuggestions.begin(); it != userPriceSuggestions.end(); ++it){
+
+    int length = priceSuggestions.size();
+    if(length == 0){
+        //cout << "NOTE: Saving user " << userID << "rating: " << userRating <<endl;
+        user.userRating = userRating;
+        userGlobal = user; // update
+        return;
+    }
+
+    //for (list<priceSuggestion>::iterator it=userPriceSuggestions.begin(); it != userPriceSuggestions.end(); ++it){
+    list<priceSuggestion>::iterator it = userPriceSuggestions.begin();
+    while(it != userPriceSuggestions.end()) {
         if((*it).rating>10){
             userRating++;
         }
         else if ((*it).rating<-10){
             userRating--;
         }
+        ++it;
     }
-    cout<<"Rating:"<<userRating<<endl;
-    cout<<"M010"<<endl;
+    //cout << "NOTE: Saving user " << userID << "rating: " << userRating <<endl;
+    user.userRating = userRating;
+    userGlobal = user; //update
+    cout << "Uzticamības reitings veiksmīgi atjaunināts." <<endl;
 }
 
-int main(){
-    priceSuggestions.push_back(priceSuggestion(1, 11));
-    priceSuggestions.push_back(priceSuggestion(1, 3));
-    priceSuggestions.push_back(priceSuggestion(1, 4));
-    priceSuggestions.push_back(priceSuggestion(1, 5));
-    priceSuggestions.push_back(priceSuggestion(1, 6));
+int main() {
+    // priceSuggestions.push_back(priceSuggestion(userID, userRating))
+    // Šeit ir domāts, ka userID ir tikai viens vienīgs, t.i. arguments.
+    // Jo tas ir domāts kā query, nevis pilnā datubāze
+    priceSuggestions.push_back(priceSuggestion(USERID, 11));
+    priceSuggestions.push_back(priceSuggestion(USERID, 3));
+    priceSuggestions.push_back(priceSuggestion(USERID, 4));
+    priceSuggestions.push_back(priceSuggestion(USERID, 5));
+    priceSuggestions.push_back(priceSuggestion(USERID, 6));
 
-    for (list<priceSuggestion>::iterator it=priceSuggestions.begin(); it != priceSuggestions.end(); ++it)
-        cout<<(*it).userID<<" "<<(*it).rating<<endl;
-    cout << '\n';
+    // Šī ir vienkārši izprintēšana, varbūt palīdz saprast
+    // for (list<priceSuggestion>::iterator it=priceSuggestions.begin(); it != priceSuggestions.end(); ++it)
+    //     cout<<(*it).userID<<" "<<(*it).rating<<endl;
+    // cout << '\n';
     
-    int selectedUserID = 1; // Selected userID value for test
+    // Ja grib simulēt, ka nav cenu piedāvājumu, atkomentēt nākamo līniju:
+    //priceSuggestions.clear();
+
+    // Ja grib simulēt, ka neeksistē tāds lietotājs datubāzē
+    // nomaina selectedUserID uz kaut ko, kas nav vienāds ar USERID
+    cout << "User rating before: " << userGlobal.userRating << endl;
+    int selectedUserID = USERID;
     updateUserRating(selectedUserID);
+    cout << "User rating after: " << userGlobal.userRating << endl;
 }
