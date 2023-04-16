f903bcc087beb3da05d05d04980744c291fc874c
askolds <askolds11@gmail.com>
2023-04-16 22:29:24 +0300

    tt

--- musuFunkcija/updateUserRating.h
+++ musuFunkcija/updateUserRating.h
@@ -3,17 +3,19 @@
 // UserID, ko liktu queryā.
 // Sarakstā nebūtu jāeksistē cita tipa ID!
 // funkcijā iegūtu sarakstu un lietotāju tikai ar šo ID, ja būtu DB
-const int USERID = 2;
+// const int USERID = 2;
 
 // Cenas piedāvājuma objekts
 struct priceSuggestion {
-  int rating;
-  int userID;
-  priceSuggestion(int userID, int rating);
+public:
+    int rating;
+    int userID;
+    priceSuggestion(int userID, int rating);
 };
 
 // Lietotāja objekts
 struct User {
+public:
     int userID;
     int userRating;
     User(int userID, int userRating);
@@ -22,7 +24,20 @@ struct User {
 // query = $"SELECT * FROM priceSuggestions WHERE userID = $userID"
 // query2 = $"SELECT * FROM Users WHERE userID = $userID"
 // simulē query, ja būtu īsta datubāze
-std::list<priceSuggestion> priceSuggestions;
-User userGlobal(USERID, 10);
 
-void updateUserRating(int userID);
+
+class test {
+public:
+    std::list<priceSuggestion> priceSuggestions;
+    //User userGlobal(USERID, 10);
+    User userGlobal;
+    int USERID;
+    int initialRating;
+    test(int USERID, int initialRating) : userGlobal(USERID, initialRating) {
+        this->USERID = USERID;
+        this->initialRating = initialRating;
+    }
+    ~test();
+
+    void updateUserRating(int userID);
+};
